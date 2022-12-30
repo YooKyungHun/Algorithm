8a6583e2fbbd83472f8f3f354537102b41c7be2f
ygeum2 <ygeum1@gmail.com>
2022-12-31 05:45:29 +0900

    20221230

---
+++ test2.py
@@ -0,0 +1,11 @@
+from collections import deque
+arr = [(0, 3), (1, 2), (2, 1), (3, -3), (4, -1)]
+arr = deque(arr)
+distance = 1001
+arr.rotate(-distance)
+
+print(arr)
+
+
+print(-5 % 3)
+print(5 % 3)
